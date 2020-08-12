72fe150a59bdb4215ec2b5f13f9097ebc040c44a
MariusS94 <marius1spomer@gmail.com>
2020-08-12 22:57:36 +0200

    Add listItem listItemTest listItemIcon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
-import List from "./components/list";
+import List from "./components/List";
+import ListItem from "./components/ListItem";
+import ListItemIcon from "./components/ListItemIcon";
+import bisasam from "./images/bisasam.png";
+import ListItemText from "./components/ListItemText";
 
 function App() {
   return (
     <div className="app">
       <header className="app__header">
-        <h2>Pokodex</h2>
-        <input placeholder="Search" />
+        Pokedexs
+        <input placeholder="Search" className="header__input" />
       </header>
       <main className="app__main">
-        <List>Dies ist ein Children von List</List>
+        <List>
+          <ListItem href="#">
+            <ListItemIcon image={bisasam} />
+            <ListItemText primary="Bisasam" secondary="#002" />
+            <ListItemIcon image="https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fire.svg" />
+          </ListItem>
+        </List>
       </main>
-      <footer className="app_footer">
-        <p>smth</p>
-      </footer>
+      <footer className="app__footer">Placeholder</footer>
     </div>
   );
 }
