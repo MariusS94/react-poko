9a539760623618afb9cbdc0a1a823e531e6ad86f
MariusS94 <marius1spomer@gmail.com>
2020-08-12 13:36:44 +0200

    Add grid and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import List from "./components/list";
 
 function App() {
-  return <div>Pokodex</div>;
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h2>Pokodex</h2>
+        <input placeholder="Search" />
+      </header>
+      <main className="app__main">
+        <List>Dies ist ein Children von List</List>
+      </main>
+      <footer className="app_footer">
+        <p>smth</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
