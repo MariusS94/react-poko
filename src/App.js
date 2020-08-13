c8e63c705667f00e683b06664871e6ed22839fd5
MariusS94 <marius1spomer@gmail.com>
2020-08-13 16:50:51 +0200

    Add useEffect and loadingscreen

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import List from "./components/List";
 import ListItem from "./components/ListItem";
 import ListItemIcon from "./components/ListItemIcon";
+import { loading } from "./assets/loading.svg";
 
 import ListItemText from "./components/ListItemText";
+import { fetchPokes } from "./api/Pokemon";
 
-const bulbasaur = {
-  name: "Bulbasaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
-  id: "001",
-  link: "#bulbasaur",
-};
-const ivysaur = {
-  name: "Ivysaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
-  id: "002",
-  link: "#ivysaur",
-};
-const pokemons = [bulbasaur, ivysaur];
+function waitFor(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 function App() {
+  const [pokemons, setPokemons] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  /* const handleClick = async () => {
+    const allPokemons = await fetchPokes();
+    setPokemons(allPokemons);
+  
+  }; */
+
+  useEffect(() => {
+    async function fetchData() {
+      await waitFor(2000);
+      const allPokemons = await fetchPokes();
+      setIsLoaded(true);
+      setPokemons(allPokemons);
+    }
+    fetchData();
+  }, []);
+
+  /* useEffect(() => {
+    const timer = setTimeout async () => (
+      const allPokemons = await fetchPokes();
+      setPokemons(allPokemons);
+      
+  }, 1000);
+    return () => clearTimeout(timer);
+}, []); */
+  if (!isLoaded) {
+    return <img src={loading} alt="loading spinner" />;
+  }
   return (
     <div className="app">
       <header className="app__header">
         Pokedexs
         <input placeholder="Search" className="header__input" />
+        {/* <button onClick={handleClick}>generate pokemon</button> */}
       </header>
       <main className="app__main">
         <List>
-          {pokemons.map((pokemon) => (
+          {pokemons?.map((pokemon) => (
             <ListItem key={pokemon.id} href={pokemon.link}>
               <ListItemIcon image={pokemon.imgSrc} />
               <ListItemText
